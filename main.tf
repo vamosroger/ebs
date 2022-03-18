a49b9a7c51be5b4fd341645bca0062060c6e2200
terraform <arnold.ramos04@gmail.com>
2022-03-18 19:05:17 +0800

    updated variables and values

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_security_group_rule" "elb_oub_https" {
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  cidr_blocks       = data.aws_vpc.vpc.cidr_block
+  cidr_blocks       = [data.aws_vpc.vpc.cidr_block]
   security_group_id = aws_security_group.elb_sg.id	
 }
 
@@ -37,7 +37,7 @@ resource "aws_security_group_rule" "elb_oub_http" {
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  cidr_blocks       = data.aws_vpc.vpc.cidr_block
+  cidr_blocks       = [data.aws_vpc.vpc.cidr_block]
   security_group_id = aws_security_group.elb_sg.id	
 }
 
